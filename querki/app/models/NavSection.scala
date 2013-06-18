1185033d2869f5d551c38efe0923ee6f59d0db87
Mark "Justin" Waks <jducoeur+github@gmail.com>
2013-06-18 16:39:30 -0400

    The "Space Home" link now goes to Space-as-Thing
    This should make it easier to edit the Space Page itself, which has been
    annoying me.

--- querki/app/models/NavSection.scala
+++ querki/app/models/NavSection.scala
@@ -25,7 +25,7 @@ object NavSection {
     
     val spaceSection = rc.state map { state =>
       NavSection(truncateName(state.displayName), Seq(
-        NavLink("Space Home", routes.Application.space(owner, spaceId)),
+        NavLink("Space Home", routes.Application.thing(owner, spaceId, spaceId)),
         NavLink("Create a Thing", routes.Application.createThing(owner, spaceId, None), Some("createThing")),
         NavLink("Add a Property", routes.Application.createProperty(owner, spaceId)),
         NavLink("Upload a Photo", routes.Application.upload(owner, spaceId)),
