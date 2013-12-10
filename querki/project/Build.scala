f3a801f5c6f3ef538598480051813d4af76da79b
Mark "Justin" Waks <jducoeur+github@gmail.com>
2013-12-10 15:45:45 -0500

    Release 0.4.4.3

--- querki/project/Build.scala
+++ querki/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "Querki"
-    val appVersion      = "0.4.4.2"
+    val appVersion      = "0.4.4.3"
 
     val appDependencies = Seq(
 	  // Main Play dependencies
