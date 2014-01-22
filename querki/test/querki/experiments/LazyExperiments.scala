bc4639cc8f912427c850bc461f22896f3cf962b8
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-01-22 10:27:15 -0500

    Experiment to prove that lazy vals are okay in case classes
    I had realized that I was making an important assumption: that when you
    copy a case class, it would re-evaluate the lazy vals.  That seemed
    reasonable, but was too important an assumption to leave untested.
    
    Fortunately, it works.  This will probably have important ramifications
    for optimizing the usage of SpaceStates.

---
+++ querki/test/querki/experiments/LazyExperiments.scala
@@ -0,0 +1,19 @@
+package querki.experiments
+
+import querki.test._
+
+class LazyExperiments extends QuerkiTests {
+  "A case class" should {
+    "re-evaluate its lazy vals when copied" in {
+      case class MyClass(nums:Seq[Int]) {
+        lazy val total:Int = nums.sum
+      }
+      
+      val first = MyClass(Seq(1, 2, 3))
+      assert(first.total == 6)
+      
+      val second = first.copy(nums = first.nums :+ 4)
+      assert(second.total == 10)
+    }
+  }
+}
