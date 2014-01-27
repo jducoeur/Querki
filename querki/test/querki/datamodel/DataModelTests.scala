264c42295d93076ec6d543907e1e06a53fd8ca3d
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-01-27 10:15:48 -0500

    Added unit tests for _is

--- querki/test/querki/datamodel/DataModelTests.scala
+++ querki/test/querki/datamodel/DataModelTests.scala
@@ -3,6 +3,7 @@ package querki.datamodel
 import querki.test._
 
 class DataModelTests extends QuerkiTests {
+  // === _hasProperty ===
   "_hasProperty" should {
     "produce true iff the Thing has the Property" in {
       processQText(commonThingAsContext(_.instance), """[[_hasProperty(My Optional Text._self)]]""") should 
@@ -27,6 +28,39 @@ class DataModelTests extends QuerkiTests {
     "error if the parameter isn't a Thing" in {
       processQText(commonThingAsContext(_.instance), """[[_hasProperty(My Optional Text)]]""") should 
         equal (expectedWarning("Func.paramNotThing"))      
+    }
+  }
+  
+  // === _is ===
+  "_is" should {
+    "successfully check an incoming Thing" in {
+      processQText(commonThingAsContext(_.instance), """[[_is(My Instance)]]""") should 
+        equal ("""true""")
+    }
+    
+    "work inside _if" in {
+      processQText(commonThingAsContext(_.instance), """[[_if(_is(My Instance), ""hello"")]]""") should 
+        equal ("""hello""")      
+    }
+    
+    "correctly fail the wrong Thing" in {
+      processQText(commonThingAsContext(_.sandbox), """[[_is(My Instance)]]""") should 
+        equal ("""false""")
+    }
+
+    "work on a Link Property" in {
+      class TSpace extends CommonSpace {
+        val linkee = new SimpleTestThing("Linkee")
+        val linker = new SimpleTestThing("Linker", singleLinkProp(linkee))
+        val other = new SimpleTestThing("Other")
+      }
+      val space = new TSpace
+      
+      processQText(thingAsContext[TSpace](space, (_.linker)), """[[_if(Single Link -> _is(Linkee), ""Yes"", ""No"")]]""") should
+        equal ("""Yes""")
+      
+      processQText(thingAsContext[TSpace](space, (_.linker)), """[[_if(Single Link -> _is(Other), ""Yes"", ""No"")]]""") should
+        equal ("""No""")
     }
   }
 }
