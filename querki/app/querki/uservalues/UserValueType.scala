b46634a0b6f75677e2cf2c6fbffa0d0e63f48e6c
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-04-18 15:18:56 -0400

    Fixed the underlying UserValueType

--- querki/app/querki/uservalues/UserValueType.scala
+++ querki/app/querki/uservalues/UserValueType.scala
@@ -7,7 +7,9 @@ import models.Thing.PropFetcher
 
 import querki.core.TypeUtils.SystemType
 import querki.ecology._
-import querki.values.{ElemValue, QLContext, RequestContext, SpaceState}
+import querki.values.{ElemValue, QLContext, QValue, RequestContext, SpaceState}
+
+case class UserValueWrapper[UVT,ST](userValue:Option[QValue], summary:Option[ST])
 
 /**
  * A specialized kind of PType that presents two faces. To the user, it is a User Value,
@@ -17,17 +19,26 @@ import querki.values.{ElemValue, QLContext, RequestContext, SpaceState}
  * 
  * Ratings and Reviews are the archetypal UserValueTypes.
  */
-abstract class UserValueType[UVT, VT](tid:OID, pf:PropFetcher)(implicit e:Ecology) extends SystemType[VT](tid,pf) {
+abstract class UserValueType[UVT, ST](tid:OID, pf:PropFetcher)(implicit e:Ecology) extends SystemType[UserValueWrapper[UVT,ST]](tid,pf) {
+  
+  type Wrapper = UserValueWrapper[UVT,ST]
+  
+  def extractUserDisplayPropVal(dpv:DisplayPropVal):DisplayPropVal = {
+    val wrapperOpt = dpv.v.flatMap(_.firstAs(this))
+    wrapperOpt.map(wrapper => dpv.copy(v = wrapper.userValue)).getOrElse(dpv)
+  }
   
   /**
    * Note that these all operate on the *summary*, not the userValue itself.
    */
-  def doDeserialize(ser:String)(implicit state:SpaceState):VT = summarizer.doDeserialize(ser)
-  def doSerialize(v:VT)(implicit state:SpaceState):String = summarizer.doSerialize(v)
-  def doWikify(context:QLContext)(v:VT, displayOpt:Option[Wikitext] = None):Wikitext = summarizer.doWikify(context)(v, displayOpt)
-  def doDefault(implicit state:SpaceState):VT = summarizer.doDefault
-  // You can't input summaries:
-  override def renderInputXml(prop:Property[_,_], rc:RequestContext, currentValue:DisplayPropVal, v:ElemValue):NodeSeq = NodeSeq.Empty
+  def doDeserialize(ser:String)(implicit state:SpaceState):Wrapper = UserValueWrapper(None, Some(summarizer.doDeserialize(ser)))
+  def doSerialize(v:Wrapper)(implicit state:SpaceState):String = v.summary.map(summarizer.doSerialize(_)).getOrElse("")
+  def doWikify(context:QLContext)(v:Wrapper, displayOpt:Option[Wikitext] = None):Wikitext = 
+    v.summary.map(summarizer.doWikify(context)(_, displayOpt)).getOrElse(Wikitext.empty)
+  def doDefault(implicit state:SpaceState):Wrapper = UserValueWrapper(None, Some(summarizer.doDefault))
+  // *Editing* a UserValueType means editing the underlying userType.
+  override def renderInputXml(prop:Property[_,_], rc:RequestContext, currentValue:DisplayPropVal, v:ElemValue):NodeSeq = 
+    userType.renderInputXml(prop, rc, extractUserDisplayPropVal(currentValue), v)
   
   // TODO: go through the rest of the PType methods, and see what else should be delegated to userType or summarizer
   
@@ -42,5 +53,5 @@ abstract class UserValueType[UVT, VT](tid:OID, pf:PropFetcher)(implicit e:Ecolog
    * 
    * Must be defined by the concrete class.
    */
-  def summarizer:Summarizer[UVT,VT]
+  def summarizer:Summarizer[UVT,ST]
 }
