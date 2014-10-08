5ab81795f40838b0d81539492b7820ade2510e3f
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-10-08 09:36:55 -0400

    Fixing some problems with Manifest
    Unfortunately, the data passed in from Manifest is not as consistent as
    I could wish...

--- querki/scalajs/src/main/scala/querki/display/input/TagSetInput.scala
+++ querki/scalajs/src/main/scala/querki/display/input/TagSetInput.scala
@@ -19,7 +19,7 @@ import ManifestFacade._
 
 trait ManifestItem extends js.Object {
   def display:String = ???
-  def id:String = ???
+  def id:js.UndefOr[String] = ???
 }
   
 class TagSetInput(val rawElement:dom.Element)(implicit e:Ecology) extends InputGadget(e) {
@@ -35,9 +35,13 @@ class TagSetInput(val rawElement:dom.Element)(implicit e:Ecology) extends InputG
   lazy val entryPoint = if (isNames) "getTags" else "getLinks"
   lazy val required = if (isNames) false else true
   lazy val typeName = if (isNames) "tag" else "thing"
+    
+    println(s"isNames = $isNames; required = $required")
   
+  // The constructor for the Manifest object itself. This prompts you when you start typing,
+  // using MarcoPolo, and organizes results into a nice list.
   $(element).manifest(lit(
-    // TODO: marcoPolo should be going through the new Client system!
+    // TODO: marcoPolo should be going through the ClientController!
     marcoPolo = lit(
       url = s"$entryPoint?propId=$propId",
       minChars = 1,
@@ -46,19 +50,21 @@ class TagSetInput(val rawElement:dom.Element)(implicit e:Ecology) extends InputG
       formatItem = { (data:ManifestItem) => data.display },
       formatNoResults = { (q:String) => s"No existing $typeName with <b>$q</b> found." }
     ),
-    formatDisplay = { (data:ManifestItem) => data.display },
-    formatValue = { (data:ManifestItem) => data.id },
-    separator = Array[Char](13, ','),
+    // Yes, these two are horrible, but represent an unfortunate quirk of Manifest: these sometimes get called with
+    // items, sometimes with Strings:
+    formatDisplay = { (data:js.Any) => if (data.isInstanceOf[js.prim.String]) data else data.asInstanceOf[ManifestItem].display },
+    formatValue = { (data:js.Any) => if (data.isInstanceOf[js.prim.String]) data else data.asInstanceOf[ManifestItem].id },
+//    separator = Array[Char](13, ','),
     values = initialValuesJs,
     required = required
   ))
   
   // TBD: do we need an unhook, to avoid leaks?
   def hook() = {
-    $(element).change({ event:JQueryEventObject =>
-      println("Have changed the Manifest!")
+//    $(element).change({ event:JQueryEventObject =>
+//      println("Have changed the Manifest!")
 //      saveChange(List(element.value))
-    })
+//    })
   }
   
   def doRender() =
