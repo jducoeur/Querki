ad7371485a67f8cd4876b65e01ff3048ee900571
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-10-14 15:29:14 -0400

    Added comments.

--- querki/scalajs/src/test/scala/querki/display/input/TagSetInputTests.scala
+++ querki/scalajs/src/test/scala/querki/display/input/TagSetInputTests.scala
@@ -39,6 +39,7 @@ object TagSetInputTests extends ThingPageTests with ScalatagUtils {
       val propPath = "v-linksetpropoid-MyThingId"
       
       setupPage(
+        // Guts of the Thing Page:
         div(
           input(cls:="_tagSetInput propEditor",
             tpe:="text",
@@ -48,8 +49,12 @@ object TagSetInputTests extends ThingPageTests with ScalatagUtils {
             name:=propPath
           )
         ),
+        // Stub for the marcoPolo entry point, so we can have a URL:
         Some({controllers => controllers.ClientController.marcoPolo = { entryPoint1("marcoPolo") _ }})
       )
+      // This will be called when we trigger the manifestchange -- it is the stub for the server call to save the
+      // value.
+      // TODO: this should get refactored out, since testing any InputGadget is going to want to do this.
       registerApiHandler[EditFunctions]("alterProperty")(new EditFunctionsEmpty with AutowireHandler {
         import EditFunctions._
         override def alterProperty(thingId:String, path:String, change:PropertyChange):PropertyChangeResponse = {
@@ -67,8 +72,14 @@ object TagSetInputTests extends ThingPageTests with ScalatagUtils {
       
       // Now -- can we trigger a change, and get the save message out?
       val promise = Promise[String]
+      // savecomplete is triggered when InputGadget receives a PropertyChanged from the server.
+      // We need to set it now, because things happen synchronously in utest:
       manifestBase.on("savecomplete", { () => promise.success("Got it") })
+      // Tell the TagSetInput Gadget that the Manifest has changed:
+      // TODO: once we figure out how to load Manifest's Javascript in tests, try to do this more
+      // realistically:
       manifestBase.trigger("manifestchange", Seq("My First Tag", "My Second Tag").toJSArray)
+      // Wait to be told that we're gotten to savecomplete:
       promise.future.map { result =>
         assert(result == "Got it")
       }
