5774a8381789b992b3a82facde25f36f67a32f98
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-12-01 14:40:15 -0500

    Beginning to do drag-and-drop Instance Properties
    Nowhere near done, but at least we are able to drag between the two
    lists.

--- querki/scalajs/src/main/scala/querki/editing/ModelDesignerPage.scala
+++ querki/scalajs/src/main/scala/querki/editing/ModelDesignerPage.scala
@@ -3,6 +3,7 @@ package querki.editing
 import scala.scalajs.js
 import org.scalajs.dom
 import org.scalajs.jquery._
+import org.scalajs.jqueryui._
 import scalatags.JsDom.all._
 import autowire._
 
@@ -12,6 +13,7 @@ import querki.api.EditFunctions
 import EditFunctions._
 import querki.data.ThingInfo
 import querki.display.RawSpan
+import querki.display.input.InputGadget
 import querki.pages._
 
 class ModelDesignerPage(params:ParamMap)(implicit e:Ecology) extends Page(e) with EcologyMember  {
@@ -20,16 +22,44 @@ class ModelDesignerPage(params:ParamMap)(implicit e:Ecology) extends Page(e) wit
   
   lazy val Client = interface[querki.client.Client]
   
-  def makeEditor(info:PropEditInfo):Modifier = {
+  def makeEditor(instance:Boolean, info:PropEditInfo):Modifier = {
     val prompt = info.prompt.map(_.raw.toString).getOrElse(info.displayName)
     // TODO: there is a nasty bug here. The tooltip should be normally wiki-processed,
     // but there is no way to use raw() on an attribute value. So we instead are displaying
     // the raw, unprocessed form, knowing that Scalatags will escape it.
     val tooltip = info.tooltip.map(_.plaintext).getOrElse(info.displayName)
-    div(cls:="_withTooltip", title:=tooltip,
+    li(cls:="_withTooltip", title:=tooltip,
       raw(s"$prompt (${info.propId}): "),
       new RawSpan(info.editor)
     )
+  }
+  
+  class PropertySection(nam:String, instances:Boolean, props:Seq[PropEditInfo]) extends InputGadget[dom.HTMLUListElement](ecology) {
+    def values = ???
+    
+    def hook() = {
+      $(elem).find("._propertySection").each({ prop:dom.Element =>
+      }:js.ThisFunction0[dom.Element, Any])
+
+      $(elem).sortable(SortableOptions.
+        connectWith("._propertySection").
+        // Stop gets called after a drag-and-drop event:
+        stop({ (evt:JQueryEventObject, ui:SortChangeUI) =>
+          val item = ui.item.get
+          val nowIn = item.parent
+          println(s"Moved from $instances to ${nowIn.data("instances")}")
+//        val oldIndex = item.data("index").asInstanceOf[Int]
+//        val newIndex = sortList.children("li").index(item)
+//        saveChange({ path => MoveListItem(path, oldIndex, newIndex) })
+//        numberItems()
+        }:js.Function2[JQueryEventObject, SortChangeUI, Any]
+      ))
+    }
+    
+    def doRender() = 
+      ul(cls:="_propertySection", name:=nam, data("instances"):=instances,
+        props.map(makeEditor(instances, _))
+      )
   }
 
   def pageContent = {
@@ -49,11 +79,11 @@ class ModelDesignerPage(params:ParamMap)(implicit e:Ecology) extends Page(e) wit
           p("Drag and drop Properties by their name to rearrange them."),
           h3("Instance Properties"),
           p("These are the Properties that can be different for each Instance"),
-          sortedInstanceProps.map(makeEditor(_)),
+          new PropertySection("instanceProps", true, sortedInstanceProps),
           querkiButton("Add a Property"),
           h3("Model Properties"),
           p("These are the Properties that are the same for all Instances of this Model"),
-          modelProps.map(makeEditor(_))
+          new PropertySection("modelProps", false, modelProps)
         )
     }
       yield PageContents(s"Designing Model ${model.displayName}", guts)
