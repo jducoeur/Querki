76dd99fb59e898aeb81498d66680f3b5f4199e4f
Mark "Justin" Waks <jducoeur+github@gmail.com>
2014-12-09 13:44:25 -0500

    Pulled a couple of Gadgets into their own files

---
+++ querki/scalajs/src/main/scala/querki/display/ButtonGadget.scala
@@ -0,0 +1,35 @@
+package querki.display
+
+import org.scalajs.dom
+import org.scalajs.jquery._
+import scalatags.JsDom.all._
+
+import querki.globals._
+
+object ButtonKind extends Enumeration {
+  type ButtonKind = Value
+  
+  val Normal, Info, Primary = Value
+}
+import ButtonKind._
+
+/**
+ * A simple Gadget that encapsulates the notion of a Button that does something when clicked. Almost
+ * trivial, but this saves some boilerplate.
+ */
+class ButtonGadget(kind:ButtonKind, mods:Modifier*)(onClick: => Unit) extends Gadget[dom.HTMLAnchorElement] {
+  def doRender() = {
+    val kindStr = kind match {
+      case Normal => ""
+      case Info => "btn-info"
+      case Primary => "btn-primary"
+    }
+    a(cls:=s"btn $kindStr", mods)
+  }
+  
+  override def onCreate(e:dom.HTMLAnchorElement) = {
+    $(elem).click({ evt:JQueryEventObject =>
+      onClick
+    })
+  }
+}
