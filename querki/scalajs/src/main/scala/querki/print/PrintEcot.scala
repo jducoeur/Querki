d449904f90a9114cf82076d3c5995cf2cda2e70d
Mark "Justin" Waks <jducoeur+github@gmail.com>
2015-02-03 14:43:08 -0500

    We now copy all of the styles into the Print View
    This proves to be tricky, because we have to wait for all of the
    stylesheet links to fully load before printing.

--- querki/scalajs/src/main/scala/querki/print/PrintEcot.scala
+++ querki/scalajs/src/main/scala/querki/print/PrintEcot.scala
@@ -41,8 +41,26 @@ class PrintEcot(e:Ecology) extends ClientEcot(e) with Print {
     $("#_pageStyles").each({ (e:dom.Element) =>
       $(printDoc.head).append($(e).clone())
     }:js.ThisFunction0[dom.Element, Any])
+    // This part is tricky: we want to copy in all of the stylesheets, but then we have to wait until they
+    // finish loading before we actually print.
+    // IMPORTANT: note the explicit assumption that the number of links is non-zero!
+    var outstandingLinks = 0
+    $("link").filter({ (index:Int, e:dom.Element) =>
+      $(e).attr("rel") == "stylesheet"
+    }).each({ (e:dom.Element) => 
+      val newLink = $(e).clone()
+      outstandingLinks += 1
+      $(printDoc.head).append(newLink)
+      $(newLink).on("load", null, null, { e:JQueryEventObject => 
+        outstandingLinks -= 1
+        if (outstandingLinks == 0) {
+          // Okay, all of the links have loaded; we can now actually do the printing:
+          printFrame.elem.contentWindow.print()
+        }
+        1:js.Any 
+      })
+    }:js.ThisFunction0[dom.Element, Any])
     $(printDoc.body).append(new QText(printView, cls:="_printView").render)
-    printFrame.elem.contentWindow.print()
     // TODO: in principle, we should hook afterprint and remove the iframe. Not a major concern, though.
   }
 
