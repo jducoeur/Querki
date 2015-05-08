4993978b62563179f448191f46c828d52bb18b5c
Mark "Justin" Waks <jducoeur+github@gmail.com>
2015-05-08 18:08:04 -0400

    Fixed bug .3y286ph
    Turns out that ViewPhotoDialog hadn't been updated to deal with
    Bootstrap 3.0.

--- querki/scalajs/src/main/scala/querki/photos/ViewPhotoDialog.scala
+++ querki/scalajs/src/main/scala/querki/photos/ViewPhotoDialog.scala
@@ -15,14 +15,13 @@ class ViewPhotoDialog extends Gadget[dom.HTMLDivElement] {
 
   def showFrom(thumbnail:Thumbnail) = {
     // TODO: yes, there's a lot of horrible hardcoded magic here. Can we use CSS to make this less awful?
-    $(elem)
+    $(elem).find(".modal-dialog")
       .width(thumbnail.fullWidth + 31)
       .height(thumbnail.fullHeight + 50)
       // TBD: can we use Scalatags here somehow?
       .css(js.Dictionary[js.Any](
-        "max-height" -> "100%",
-        "margin-left" -> s"-${thumbnail.fullWidth / 2}px"
-      ))
+        "max-height" -> "100%"
+      ))  
       
     $(photoImage.elem).attr("src", thumbnail.fullSrc).width(thumbnail.fullWidth).height(thumbnail.fullHeight)
     $(elem).modal(ModalCommand.show)
@@ -31,17 +30,32 @@ class ViewPhotoDialog extends Gadget[dom.HTMLDivElement] {
   lazy val photoImage = Gadget(img())
 
   def doRender() =
-    div(cls:="modal hide",
+    div(cls:="modal",
+      tabindex:="-1",
+      role:="dialog",
+      aria.labelledby:="View Photo",
+      aria.hidden:="true",
       title:="View Photo",
       display:="none",
-      div(id:="photo-full-header", cls:="modal-header",
-        // TODO: add aria.hidden:="true" -- Aria support is new in Scalatags:
-        button(tpe:="button", cls:="close", data("dismiss"):="modal", "x"),
-        p(b("View Image"))
-      ),
-      div(cls:="modal-body",
-        maxHeight:="100%",
-        photoImage
+      div(
+        cls:="modal-dialog",
+        div(
+          cls:="modal-content",
+          div(id:="photo-full-header", cls:="modal-header",
+            button(
+              tpe:="button", 
+              cls:="close", 
+              data("dismiss"):="modal",
+              aria.label:="Close",
+              span(aria.hidden:="true", raw("&times;"))
+            ),
+            p(cls:="modal-title", b("View Image"))
+          ),
+          div(cls:="modal-body",
+            maxHeight:="100%",
+            photoImage
+          )
+        )
       )
     )
 }
