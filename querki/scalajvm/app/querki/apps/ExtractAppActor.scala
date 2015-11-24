64b5f568800d4fdf3d9cd8870a3427385f81f468
Mark "Justin" Waks <jducoeur+github@gmail.com>
2015-11-23 19:11:03 -0500

    Checkpoint: first draft of Extractees is done

--- querki/scalajvm/app/querki/apps/ExtractAppActor.scala
+++ querki/scalajvm/app/querki/apps/ExtractAppActor.scala
@@ -9,7 +9,11 @@ import models._
 import querki.api.ProgressActor
 import querki.data.TID
 import querki.globals._
+import querki.identity.User
+import querki.time.DateTime
+import querki.types.ModelTypeBase
 import querki.util.QuerkiActor
+import querki.values.SpaceState
 
 /**
  * The Actor that shepherds the process of extracting a new App from an existing Space. This is separate
@@ -19,16 +23,21 @@ import querki.util.QuerkiActor
  * 
  * @author jducoeur
  */
-private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], state:SpaceState) 
+private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], name:String, owner:User, state:SpaceState) 
   extends Actor with Requester with EcologyMember with ProgressActor 
 {
+  lazy val Core = interface[querki.core.Core]
   lazy val SpacePersistence = interface[querki.spaces.SpacePersistence]
+  lazy val System = interface[querki.system.System]
+  
+  lazy val SystemSpace = System.State
+  lazy val systemId = SystemSpace.id
   
   implicit val s = state
   implicit val ec = ecology
   
   // The Things that need to be Extracted. Note that typeModels is a subset of models.
-  case class Extractees(instances:Set[OID], models:Set[OID], props:Set[OID], types:Set[OID], typeModels:Set[OID])
+  case class Extractees(state:SpaceState, typeModels:Set[OID])
   var _extractees:Option[Extractees] = None
   
   override def receive = handleRequestResponse orElse handleProgress
@@ -75,15 +84,36 @@ private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], sta
   /**
    * Creates the complete Extractees structure, with all the stuff we expect to pull out.
    * 
-   * Note that this explicitly assumes there are no loops involved in the Model Types.
-   * TODO: is this a safe assumption? Maybe not. Investigate...
+   * Note that this explicitly assumes there are no loops involved in the Model Types. That's a
+   * fair assumption -- a lot of things will break if there are -- but do we need to sanity-check
+   * for that?
    */
   def computeExtractees():Extractees = {
-    ???
+    val oids = elements
+      .map(tid => ThingId(tid.underlying))
+      .collect { case AsOID(oid) => oid }
+    
+    val init = Extractees(SpaceState(
+      OID(1, 1),
+      systemId,
+      Map(Core.NameProp(name)),
+      owner.mainIdentity.id,
+      name,
+      DateTime.now,
+      Seq.empty,
+      Some(SystemSpace),
+      Map.empty,
+      Map.empty,
+      Map.empty,
+      Map.empty,
+      None
+    ), Set.empty)
+    
+    (init /: oids) { (ext, elemId) => addThingToExtract(elemId, ext) }
   }
   
   def addThingToExtract(id:OID, in:Extractees):Extractees = {
-    if (in.instances.contains(id) || in.models.contains(id))
+    if (in.state.things.contains(id))
       // Already done
       in
     else {
@@ -93,10 +123,7 @@ private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], sta
             // Add all the props to the list...
             val withProps = (in /: t.props.keys) { (ext, propId) => addPropToExtract(propId, ext) }
             // ... and this thing:
-            if (t.isModel)
-              in.copy(models = in.models + id)
-            else
-              in.copy(instances = in.instances + id)
+            in.copy(state = in.state.copy(things = in.state.things + (id -> t)))
           } else
             // Not local, so don't extract it
             in
@@ -108,7 +135,7 @@ private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], sta
   }
   
   def addPropToExtract(id:OID, in:Extractees):Extractees = {
-    if (in.props.contains(id))
+    if (in.state.spaceProps.contains(id))
       in
     else {
       state.prop(AsOID(id)) match {
@@ -119,7 +146,7 @@ private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], sta
             // ... the Type...
             val withType = addTypeToExtract(p.pType, withProps)
             // ... and this Prop itself:
-            withType.copy(props = withType.props + id)
+            withType.copy(state = in.state.copy(spaceProps = withType.state.spaceProps + (id -> p)))
           } else
             in
         }
@@ -128,8 +155,23 @@ private [apps] class ExtractAppActor(val ecology:Ecology, elements:Seq[TID], sta
     }
   }
   
-  def addTypeToExtract(id:PType[_], in:Extractees):Extractees = {
-    ???
+  def addTypeToExtract(pt:PType[_], in:Extractees):Extractees = {
+    if (in.state.types.contains(pt.id))
+      in
+    else {
+      if (pt.spaceId == state.id) {
+        // If this is a model type, we need to dive in and add that:
+        pt match {
+          case mt:ModelTypeBase => {
+            // We specifically note that this is a Model Type, because those do *not* get shadow copies
+            // in the new Space:
+            val withMT = in.copy(typeModels = in.typeModels + mt.basedOn)
+            addThingToExtract(mt.basedOn, withMT)
+          }
+        }
+      } else
+        in
+    }
   }
 }
 
