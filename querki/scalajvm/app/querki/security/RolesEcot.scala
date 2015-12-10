92a7682df657a09f8308813c81d7dbc7633c0c80
Mark "Justin" Waks <jducoeur+github@gmail.com>
2015-12-10 17:10:31 -0500

    .3y28abq (partial): the Roles now have Display Names, so they don't all say "Role"

--- querki/scalajvm/app/querki/security/RolesEcot.scala
+++ querki/scalajvm/app/querki/security/RolesEcot.scala
@@ -23,6 +23,7 @@ class RolesEcot(e:Ecology) extends QuerkiEcot(e) with Roles {
   
   val AccessControl = initRequires[AccessControl]
   val Apps = initRequires[querki.apps.Apps]
+  val Basic = initRequires[querki.basic.Basic]
   val Conversations = initRequires[querki.conversations.Conversations]
   val UserValues = initRequires[querki.uservalues.UserValues]
   
@@ -40,27 +41,28 @@ class RolesEcot(e:Ecology) extends QuerkiEcot(e) with Roles {
    * THINGS
    ***********************************************/
 
-  def defineRole(id:OID, name:String, desc:String, perms:Seq[Property[OID,OID]]):ThingState = ThingState(id, systemOID, AccessControl.RoleModel,
+  def defineRole(id:OID, name:String, display:String, desc:String, perms:Seq[Property[OID,OID]]):ThingState = ThingState(id, systemOID, AccessControl.RoleModel,
     toProps(
       setName(name),
+      Basic.DisplayNameProp(display),
       AccessControl.RolePermissionsProp(perms.map(_.id):_*),
       Summary(desc)))
       
   lazy val basicMemberPerms = Seq.empty[Property[OID,OID]]
   lazy val BasicMemberRole =
-    defineRole(BasicMemberOID, "Basic Member Role",
+    defineRole(BasicMemberOID, "Basic Member Role", "Basic Member",
       """Basic Member -- can read Things and Comments in this Space, but that's it.""".stripMargin,
       basicMemberPerms)
       
   lazy val commentatorPerms = Seq(AccessControl.CanReadProp, Conversations.CanReadComments, Conversations.CanComment, UserValues.UserValuePermission) ++ basicMemberPerms
   lazy val CommentatorRole =
-    defineRole(CommentatorOID, "Commentator Role",
+    defineRole(CommentatorOID, "Commentator Role", "Commentator",
       """Commentator -- can read Things, leave Comments, and provide Ratings and Reviews""".stripMargin,
       commentatorPerms)
       
   lazy val contributorPerms = Seq(AccessControl.CanCreateProp, AccessControl.CanEditChildrenProp) ++ commentatorPerms
   lazy val ContributorRole =
-    defineRole(ContributorOID, "Contributor Role",
+    defineRole(ContributorOID, "Contributor Role", "Contributor",
       """Contributor -- can do everything a Commentator can, plus create and edit Instances.""".stripMargin,
       contributorPerms)
       
@@ -68,7 +70,7 @@ class RolesEcot(e:Ecology) extends QuerkiEcot(e) with Roles {
   // TODO: once we have Moderation, add the CanModerate permission here. 
   lazy val editorPerms = Seq(AccessControl.CanEditProp) ++ contributorPerms
   lazy val EditorRole =
-    defineRole(EditorOID, "Editor Role",
+    defineRole(EditorOID, "Editor Role", "Editor",
       """Editor / Designer -- can do everything a Contributor can, plus design Models and moderate contributions from non-Members
         |
         |As of this writing, Editor doesn't do anything special, since neither of its permissions actually exist. But they
@@ -77,7 +79,7 @@ class RolesEcot(e:Ecology) extends QuerkiEcot(e) with Roles {
       
   lazy val managerPerms = Seq(Apps.CanManipulateAppsPerm) ++ editorPerms
   lazy val ManagerRole =
-    defineRole(ManagerOID, "Manager Role",
+    defineRole(ManagerOID, "Manager Role", "Manager",
       """Manager -- can do everything an Editor can, plus almost everything the Owner of the Space can do. You should only make
         |someone a Manager if you trust them completely, because they can do anything to the Space except to give it away or delete it.""".stripMargin,
       managerPerms)
