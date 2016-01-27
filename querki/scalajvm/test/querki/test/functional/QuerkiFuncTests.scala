437d821a66d3d1a2c646f3f44f0dda06db430e04
Mark "Justin" Waks <jducoeur+github@gmail.com>
2016-01-27 10:00:37 -0500

    First baby steps towards actual Functional Tests
    Eventually discovered that you need to have the native driver installed locally, and a system property pointing to it. With that done, it works.

--- querki/scalajvm/test/querki/test/functional/QuerkiFuncTests.scala
+++ querki/scalajvm/test/querki/test/functional/QuerkiFuncTests.scala
@@ -25,7 +25,27 @@ class QuerkiFuncTests
   with WebBrowser
 {
   "I should be able to open a web browser" in {
+    // Tell the underlying ChromeDriver where to find the native implementation
+    // TODO: this really belongs in a documented system property instead?
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver")
     go to "http://www.google.com/"
     quit()
   }
 }
+
+// Uncomment this and use it if ScalaTest is claiming that it "was unable to create a
+// Selenium ChromeDriver on this platform". Unfortunately, ScalaTest turns *all* exceptions
+// into that error, suppressing ChromeDriver's often-helpful exceptions.
+//
+//class DriverTests
+//  extends WordSpec
+//  with Matchers
+//  with ChromeFactory
+//{
+//  "I should be able to get a decent error" in {
+//    import org.openqa.selenium.chrome.ChromeDriver
+//    
+//    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver")
+//    new ChromeDriver()
+//  }
+//}
