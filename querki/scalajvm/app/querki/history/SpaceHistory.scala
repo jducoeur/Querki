81fb7958739a9bb9d3fcbcda0e27fb64fbf9c2a3
Mark "Justin" Waks <jducoeur+github@gmail.com>
2016-09-16 12:37:38 -0400

    Fixed SpaceHistory so that subsequent hits will update with any new events since last hit.
    Also, discovered that the events are 1-indexed, not 0.

--- querki/scalajvm/app/querki/history/SpaceHistory.scala
+++ querki/scalajvm/app/querki/history/SpaceHistory.scala
@@ -9,6 +9,8 @@ import akka.persistence.cassandra.query.scaladsl._
 import akka.persistence.query._
 import akka.stream.ActorMaterializer
 
+import org.querki.requester._
+
 import models._
 import Thing.PropMap
 
@@ -18,7 +20,7 @@ import querki.identity.User
 import querki.identity.IdentityPersistence.UserRef
 import querki.spaces.SpaceMessagePersistence._
 import querki.time._
-import querki.util.{QuerkiBootableActor, SingleRoutingParent, TimeoutChild}
+import querki.util.{QuerkiActor, SingleRoutingParent, TimeoutChild}
 import querki.values.{SpaceState, SpaceVersion}
 import querki.spaces.SpacePure
 
@@ -43,7 +45,7 @@ class SpaceHistoryParent(e:Ecology, val id:OID) extends Actor with ReceivePipeli
  * of a Space, and provides access to it.
  */
 private [history] class SpaceHistory(e:Ecology, val id:OID) 
-  extends QuerkiBootableActor(e) with SpacePure with ModelPersistence with ReceivePipeline with TimeoutChild
+  extends QuerkiActor(e) with SpacePure with ModelPersistence with ReceivePipeline with TimeoutChild
 {
   import SpaceHistory._
   
@@ -82,15 +84,12 @@ private [history] class SpaceHistory(e:Ecology, val id:OID)
   type StateHistory = Vector[HistoryRecord]
   val StateHistory = Vector
   
-  // The full history of this Space, zero-indexed.
+  // The full history of this Space, *one*-indexed. (That seems to be the way Akka Persistence works.)
+  // Be careful using this: the 1-indexing isn't at all obvious.
   // TODO: if we start to cope with extracting slices of the history, we'll need to maintain the
   // base index. Note that we can count on sequenceNr increasing monotonically (per discussion in
   // akka-persistence-cassandra Gitter, 9/13/16), so using a Vector makes sense here.
   var history = StateHistory.empty[HistoryRecord]
-  
-  override def preStart() = {
-    self ! Start
-  }
   
   implicit def OID2TID(oid:OID):TID = ClientApi.OID2TID(oid)
   
@@ -163,45 +162,52 @@ private [history] class SpaceHistory(e:Ecology, val id:OID)
     }
   }
   
-  def bootReceive = {
-    case Start => {
-      // When this Actor starts, we run through all events so far, building up an indexed StateHistory
-      // TODO: this is utterly *profligate* with RAM. Think about how we might want to restructure this in
-      // order to not hold the entire thing in memory all the time, while still providing reasonably
-      // responsive access. Should we instead rebuild stuff on-demand? Should the client signal what
-      // range of events it is looking at, and we load those in?
-      val source = readJournal.currentEventsByPersistenceId(persistenceId, 0, Int.MaxValue)
-      implicit val mat = ActorMaterializer()
-      loopback {
-        // Note that we construct the history using VectorBuilder, for efficiency:
-        source.runFold((emptySpace, new VectorBuilder[HistoryRecord])) {
-          case (((curState, history), EventEnvelope(offset, persistenceId, sequenceNr, evt))) =>
-          val nextState = evolveState(evt, curState)
-          history += HistoryRecord(sequenceNr, evt, nextState)
-          (nextState, history)
-        }
-      } foreach { fullHist =>
-        history = fullHist._2.result
-        mat.shutdown()
-        doneBooting()
+  /**
+   * This reads all of the history since the last time it was called. It is designed so that we can
+   * reload the client page and get any new events since it was last shown.
+   */
+  def readCurrentHistory():RequestM[Unit] = {
+    // TODO: this is utterly *profligate* with RAM. Think about how we might want to restructure this in
+    // order to not hold the entire thing in memory all the time, while still providing reasonably
+    // responsive access. Should we instead rebuild stuff on-demand? Should the client signal what
+    // range of events it is looking at, and we load those in?
+    val source = readJournal.currentEventsByPersistenceId(persistenceId, history.size + 1, Int.MaxValue)
+    implicit val mat = ActorMaterializer()
+    val initialState =
+      if (history.isEmpty)
+        emptySpace
+      else
+        history.last.state
+    loopback {
+      // Note that we construct the history using VectorBuilder, for efficiency:
+      source.runFold((initialState, new VectorBuilder[HistoryRecord])) {
+        case (((curState, history), EventEnvelope(offset, persistenceId, sequenceNr, evt))) =>
+        val nextState = evolveState(evt, curState)
+        history += HistoryRecord(sequenceNr, evt, nextState)
+        (nextState, history)
       }
-    }
+    } map { fullHist =>
+      history = history ++ fullHist._2.result
+      mat.shutdown()
+    }    
   }
   
   def doReceive = {
     case GetHistorySummary() => {
-      val (evtsBuilder, identityIds, thingNames) = ((new VectorBuilder[EvtSummary], Set.empty[OID], Map.empty[TID, String]) /: history) { (current, historyRec) =>
-        val (builder, identities, thingNames) = current
-        val (summary, newIdentities, newThingNames) = toSummary(historyRec.evt, historyRec.sequenceNr, identities, thingNames)(historyRec.state)
-        (builder += summary, newIdentities, newThingNames)
-      }
-      
-      loopback(IdentityAccess.getIdentities(identityIds.toSeq)) foreach { identities =>
-        val identityMap = identities.map { case (id, publicIdentity) =>
-          (id.toThingId.toString -> ClientApi.identityInfo(publicIdentity))
+      readCurrentHistory() map { _ =>
+        val (evtsBuilder, identityIds, thingNames) = ((new VectorBuilder[EvtSummary], Set.empty[OID], Map.empty[TID, String]) /: history) { (current, historyRec) =>
+          val (builder, identities, thingNames) = current
+          val (summary, newIdentities, newThingNames) = toSummary(historyRec.evt, historyRec.sequenceNr, identities, thingNames)(historyRec.state)
+          (builder += summary, newIdentities, newThingNames)
         }
         
-        sender ! HistorySummary(evtsBuilder.result, EvtContext(identityMap, thingNames))
+        loopback(IdentityAccess.getIdentities(identityIds.toSeq)) foreach { identities =>
+          val identityMap = identities.map { case (id, publicIdentity) =>
+            (id.toThingId.toString -> ClientApi.identityInfo(publicIdentity))
+          }
+          
+          sender ! HistorySummary(evtsBuilder.result, EvtContext(identityMap, thingNames))
+        }
       }
     }
   }
@@ -209,8 +215,6 @@ private [history] class SpaceHistory(e:Ecology, val id:OID)
 
 object SpaceHistory {
   def actorProps(e:Ecology, id:OID) = Props(classOf[SpaceHistoryParent], e, id)
-  
-  private case object Start
   
   sealed trait HistoryMessage
   /**
