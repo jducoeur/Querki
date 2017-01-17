68bb7049df47140aa6280f478e3879523590637c
Mark "Justin" Waks <jducoeur+github@gmail.com>
2017-01-17 16:48:20 -0500

    The simple Who Can Read radio buttons now work.

--- querki/scalajs/src/main/scala/querki/editing/EditSpaceInfoPage.scala
+++ querki/scalajs/src/main/scala/querki/editing/EditSpaceInfoPage.scala
@@ -2,12 +2,17 @@ package querki.editing
 
 import scala.concurrent.Future
 
+import org.scalajs.dom
+
 import scalatags.JsDom.all._
 import autowire._
+import rx._
 
+import querki.data.BasicThingInfo
 import querki.display._
 import querki.display.input._
 import querki.display.rx._
+import querki.editing.EditFunctions.PropertyChanged
 import querki.globals._
 import querki.pages._
 import querki.security.SecurityFunctions
@@ -21,12 +26,45 @@ class EditSpaceInfoPage(params:ParamMap)(implicit val ecology:Ecology)
   
   lazy val spaceInfo = DataAccess.space.get
   
+  /**
+   * Specialized little gadget that exists solely to save the current read setting on a Thing.
+   */
+  class ReadSaver(info:BasicThingInfo) extends InputGadget[dom.html.Span](ecology) {
+    override lazy val thingId = info.oid
+    override val path = Editing.propPath(std.security.canReadPerm, Some(info))
+    
+    def values = securityRadio.flatMap { radio =>
+      radio.selectedValOpt().map(Seq(_))
+    }.getOrElse(Seq.empty)
+    
+    override def save() = {
+      if (values.isEmpty)
+        // Don't do anything unless something is actually selected:
+        Future.successful(PropertyChanged)
+      else
+        super.save()
+    }
+    
+    def doRender = span(display := "none")
+    def hook() = {}
+  }
+  
+  val spaceSaver = GadgetRef[ReadSaver]
+  val spacePermsSaver = GadgetRef[ReadSaver]
+
   val securityRadio = GadgetRef[RxRadio]
+    .whenRendered { g =>
+      Obs(g.selectedValOpt, skipInitial=true) {
+        // Whenever the selection changes, save both objects:
+        spaceSaver.foreach(_.save())
+        spacePermsSaver.foreach(_.save())
+      }
+    }
   
   implicit class RichThingPerms(perms:ThingPermissions) {
     // true iff *both* the Space itself and the Instances are set to this level:
     def readIs(level:SecurityLevel, readPerm:PermInfo):Boolean = {
-      def find(ts:Seq[ThingPerm]) = ts.find(_.permId == std.security.canReadPerm)
+      def find(ts:Seq[ThingPerm]) = ts.find(_.permId == std.security.canReadPerm.oid)
       
       (currentPermLevel(readPerm, find(perms.perms), true) == level) &&
       (currentPermLevel(readPerm, find(perms.instancePerms), true) == level)
@@ -47,10 +85,9 @@ class EditSpaceInfoPage(params:ParamMap)(implicit val ecology:Ecology)
           std.basic.displayNameProp).zip(
         Client[SecurityFunctions].getAllPerms().call().zip(
         Client[SecurityFunctions].permsFor(spaceInfo).call()))
-      readPerm = allPerms.find{perm => println(s"Trying ${perm.id}"); perm.id.underlying == std.security.canReadPerm.underlying}.get
+      readPerm = allPerms.find { perm => perm.id.underlying == std.security.canReadPerm.underlying }.get
       isPublic = spacePerms.readIs(SecurityPublic, readPerm)
       isMembers = spacePerms.readIs(SecurityMembers, readPerm)
-      _ = println(s"Public: $isPublic Members: $isMembers")
       guts =
         div(
           h1(cls:="_defaultTitle", "Edit Space Info"),
@@ -70,6 +107,8 @@ class EditSpaceInfoPage(params:ParamMap)(implicit val ecology:Ecology)
           
           br(), p(b("Who can read this Space, broadly speaking?")),
           p(cls:="_smallSubtitle", "(You can make specific Things public or private using the Security page)"),
+          spaceSaver <= new ReadSaver(spaceInfo),
+          spacePermsSaver <= new ReadSaver(spacePerms.instancePermThing.get),
           securityRadio <= 
             new RxRadio("_securityRadio",
               oneRadioButton(SecurityPublic, "Everybody -- this Space is public", isPublic),
