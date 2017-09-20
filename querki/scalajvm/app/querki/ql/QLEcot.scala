c16402f9ef1d4338fb1984048898a7aa1d160b6e
Mark "Justin" Waks <jducoeur+github@gmail.com>
2017-09-20 18:22:24 -0400

    Improve documentation of _self, to help with confusion that Eric pointed out.

--- querki/scalajvm/app/querki/ql/QLEcot.scala
+++ querki/scalajvm/app/querki/ql/QLEcot.scala
@@ -243,8 +243,8 @@ class QLEcot(e:Ecology) extends QuerkiEcot(e) with QL with QLInternals with quer
   /***********************************************
    * FUNCTIONS
    ***********************************************/
-	
-	lazy val SelfMethod = new InternalMethod(SelfMethodOID,
+  
+  lazy val SelfMethod = new InternalMethod(SelfMethodOID,
     toProps(
       setName("_self"),
       Categories(QLTag),
@@ -253,22 +253,28 @@ class QLEcot(e:Ecology) extends QuerkiEcot(e) with QL with QLInternals with quer
           |
           |This seems silly, but it is useful for overriding the usual \_apply behavior. In particular,
           |*property*.\_self is the way to get a link to the property itself, instead of fetching the value
-          |of the property on the received Thing.
+          |of the property on the received Thing. **This is the only common use case for _self!**
           |
           |More formally, \_self is the way to override the usual \[[\_apply\]] behaviour on a Thing, to get a
           |Link to that Thing. It is never necessary for ordinary Things, but frequently useful when \_apply
           |has been defined on it.
           |
           |_self can only be used in a dotted expression -- there must always be something before the dot, and
-          |that something is what gets produced.""".stripMargin)))
-	{
-	  override def qlApply(inv:Invocation):QFut = {
+          |that something is what gets produced.
+          |
+          |If you think you want \_self for anything other than on a Property, odds are you are looking for
+          |something else. If you're trying to fetch "everything that points to this" (one of the more common
+          |cases), check out [[_refs._self]], which does exactly this. Or, if you are in a QL expression and are
+          |looking for a way to say "this Thing that I'm looking at here", you can usually get that as
+          |`$_context`. (Which is technically "the value passed into this QL expression at the beginning".)""".stripMargin)))
+  {
+    override def qlApply(inv:Invocation):QFut = {
       inv.definingContext match {
         case Some(context) => Future.successful(context.value)
         case _ => Future.successful(WarningValue("QL.self.notDotted")) 
       }
-	  }
-	}
+    }
+  }
   
   lazy val IsBoundFunction = new InternalMethod(IsBoundFunctionOID,
     toProps(
@@ -305,8 +311,8 @@ class QLEcot(e:Ecology) extends QuerkiEcot(e) with QL with QLInternals with quer
         yield ExactlyOne(YesNoType(answer))
     }
   }
-	
-	lazy val CodeMethod = new InternalMethod(CodeMethodOID,
+  
+  lazy val CodeMethod = new InternalMethod(CodeMethodOID,
     toProps(
       setName("_code"),
       Categories(QLTag),
@@ -326,90 +332,90 @@ class QLEcot(e:Ecology) extends QuerkiEcot(e) with QL with QLInternals with quer
           |
           |_code is, frankly, a bit persnickety at this point, and not always easy to use for complicated examples. It should be
           |considered a work in progress.""".stripMargin)))
-	{
-	  def encodeString(str:String):QFut = {
-	    val escaped = scala.xml.Utility.escape(str)
-	    Future.successful(HtmlUI.HtmlValue(QHtml("<pre>" + escaped + "</pre>")))    
-	  }
-	  
-	  def encode(propVal:QValue, pType:PType[_]):QFut = {
-	    if (propVal.isEmpty)
-	      WarningFut("_code got an empty input")
-	    else {
-	      pType match {
-	        case codeType:CodeType => {
-	          val str = codeType.code(propVal.first)
-	          encodeString(str)
-	        }
-	        case ParsedTextType => {
-	          encodeString(propVal.firstTyped(ParsedTextType).map(_.plaintext).getOrElse(""))
-	        }
-	        case _ => WarningFut("_code doesn't work with type " + pType.displayName)
-	      }
-	    }
-	  }
-	  
-	  def encodeThingAndProp(thing:Thing, prop:Thing)(implicit space:SpaceState):Option[QFut] = {
-	    val propAndValOpt = thing.getPropOpt(prop.id)
-	    propAndValOpt.map { propAndVal => 
-	      encode(propAndVal.v, propAndVal.prop.pType)
-	    }
-	  }
-	  
-	  // TODO: this is horrible. Surely we can turn this into something cleaner with better use of the functional
-	  // tools in the Scala toolbelt.
-	  override def qlApply(inv:Invocation):QFut = {
-	    val partialContext = inv.preferDefiningContext.context
-	    implicit val space = partialContext.state
-	    inv.paramsOpt match {
-	      case Some(params) => {
-	        // TODO: the way we're handling this is horrible and hard-coded, and needs re-examination. The thing is,
-	        // we *mostly* don't want to process this. Specifically, we don't want to process the last step of this.
-	        // For the moment, we're hard-codedly checking the first stage of the phrase and using that, but it should
-	        // probably process everything until the last stage, and return that stage.
-	        val param = params.head
-	        val stage = param.exp.phrases.head.ops.head
-	        stage match {
-	          case QLTextStage(contents, _) => encodeString(contents.reconstructString)
-	          case QLNumber(num) => encodeString(stage.reconstructString)
+  {
+    def encodeString(str:String):QFut = {
+      val escaped = scala.xml.Utility.escape(str)
+      Future.successful(HtmlUI.HtmlValue(QHtml("<pre>" + escaped + "</pre>")))    
+    }
+    
+    def encode(propVal:QValue, pType:PType[_]):QFut = {
+      if (propVal.isEmpty)
+        WarningFut("_code got an empty input")
+      else {
+        pType match {
+          case codeType:CodeType => {
+            val str = codeType.code(propVal.first)
+            encodeString(str)
+          }
+          case ParsedTextType => {
+            encodeString(propVal.firstTyped(ParsedTextType).map(_.plaintext).getOrElse(""))
+          }
+          case _ => WarningFut("_code doesn't work with type " + pType.displayName)
+        }
+      }
+    }
+    
+    def encodeThingAndProp(thing:Thing, prop:Thing)(implicit space:SpaceState):Option[QFut] = {
+      val propAndValOpt = thing.getPropOpt(prop.id)
+      propAndValOpt.map { propAndVal => 
+        encode(propAndVal.v, propAndVal.prop.pType)
+      }
+    }
+    
+    // TODO: this is horrible. Surely we can turn this into something cleaner with better use of the functional
+    // tools in the Scala toolbelt.
+    override def qlApply(inv:Invocation):QFut = {
+      val partialContext = inv.preferDefiningContext.context
+      implicit val space = partialContext.state
+      inv.paramsOpt match {
+        case Some(params) => {
+          // TODO: the way we're handling this is horrible and hard-coded, and needs re-examination. The thing is,
+          // we *mostly* don't want to process this. Specifically, we don't want to process the last step of this.
+          // For the moment, we're hard-codedly checking the first stage of the phrase and using that, but it should
+          // probably process everything until the last stage, and return that stage.
+          val param = params.head
+          val stage = param.exp.phrases.head.ops.head
+          stage match {
+            case QLTextStage(contents, _) => encodeString(contents.reconstructString)
+            case QLNumber(num) => encodeString(stage.reconstructString)
             case lit:QLListLiteral => encodeString(lit.reconstructString) 
-	          case QLCall(name, methodNameOpt, _, _) => {
-	            val thingName = name.name
-	            methodNameOpt match {
-	              case Some(methodName) => {
-	                val resultOpt = for (
-	                  thing <- space.anythingByName(thingName);
-	                  propThing <- space.anythingByName(methodName.name);
-	                  encoded <- encodeThingAndProp(thing, propThing)
-	                )
-	                  yield encoded
-	                  
-	                resultOpt.getOrElse(encodeString(param.reconstructString))
-	              }
-	              case None => {
-	                val propOpt = space.anythingByName(thingName)
-	                propOpt match {
-	                  case Some(propThing) => {
-	                    for {
-	                      thing <- inv.contextAllThings
+            case QLCall(name, methodNameOpt, _, _) => {
+              val thingName = name.name
+              methodNameOpt match {
+                case Some(methodName) => {
+                  val resultOpt = for (
+                    thing <- space.anythingByName(thingName);
+                    propThing <- space.anythingByName(methodName.name);
+                    encoded <- encodeThingAndProp(thing, propThing)
+                  )
+                    yield encoded
+                    
+                  resultOpt.getOrElse(encodeString(param.reconstructString))
+                }
+                case None => {
+                  val propOpt = space.anythingByName(thingName)
+                  propOpt match {
+                    case Some(propThing) => {
+                      for {
+                        thing <- inv.contextAllThings
                         res <- inv.fut(encodeThingAndProp(thing, propThing).getOrElse(encodeString(param.reconstructString)))
-	                    }
-	                      yield res
-	                  }
-	                  case None => encodeString(param.reconstructString)
-	                }
-	              }
-	            }
-	          }
+                      }
+                        yield res
+                    }
+                    case None => encodeString(param.reconstructString)
+                  }
+                }
+              }
+            }
             case exp:QLExpStage => encodeString(exp.reconstructString)
-	        }
-	      }
-	      case None => {
-	        encode(partialContext.value, partialContext.value.pType)
-	      }
-	    }
-	  }
-	}
+          }
+        }
+        case None => {
+          encode(partialContext.value, partialContext.value.pType)
+        }
+      }
+    }
+  }
 
   override lazy val props = Seq(
     SelfMethod,
