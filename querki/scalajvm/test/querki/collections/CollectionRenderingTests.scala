2c98fcf202e70bb03a48fc736dbc8e85496edee8
Mark "Justin" Waks <jducoeur+github@gmail.com>
2018-01-04 18:33:53 -0500

    QI.bu6ocol: unit test.

--- querki/scalajvm/test/querki/collections/CollectionRenderingTests.scala
+++ querki/scalajvm/test/querki/collections/CollectionRenderingTests.scala
@@ -80,5 +80,32 @@ class CollectionRenderingTests extends QuerkiTests {
             |</li>
             |</ul>""".stripReturns)
     }
+    
+    "render in collection-accepting text correctly" in {
+      class TSpace extends CommonSpace {
+        val myProp = new TestProperty(Core.TextType, QList, "My List")
+        val thing1 = new SimpleTestThing("Thing 1", myProp())
+        val thing2 = new SimpleTestThing("Thing 2", myProp("42"))
+        val thing3 = new SimpleTestThing("Thing 3", myProp("5", "10", "15", "20"))
+      }
+      implicit val s = new TSpace
+      
+      // Unit test for QI.bu6ocol:
+      pql("""The result is: [[Thing 1 -> My List -> *""____""]]""") should 
+        equal ("""The result is: 
+          |""".stripReturns)
+      // This line is the unit test for bug .3y2852x:
+      pql("""The result is: [[Thing 2 -> My List -> *""____""]]""") should 
+        equal ("""The result is: 
+          |
+          |42""".stripReturns)
+      pql("""The result is: [[Thing 3 -> My List -> *""____""]]""") should
+        equal ("""The result is: 
+          |
+          |5
+          |10
+          |15
+          |20""".stripReturns)
+    }
   }
 }
