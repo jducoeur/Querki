4fc37f6a68d4ee7bde8beefd8eeadae10daffeb2
Mark "Justin" Waks <jducoeur+github@gmail.com>
2018-07-23 09:28:10 -0400

    Pull PermCheckboxes out into its own file.

--- querki/scalajs/src/main/scala/querki/security/EditRolePanel.scala
+++ querki/scalajs/src/main/scala/querki/security/EditRolePanel.scala
@@ -8,11 +8,10 @@ import scalatags.JsDom.all._
 import rx._
 import autowire._
 
-import org.querki.facades.bootstrap._
 import org.querki.gadgets._
 import org.querki.jquery._
 
-import querki.api.{StandardThings, ThingFunctions}
+import querki.api.ThingFunctions
 import querki.data.{PropValInfo, ThingInfo}
 import querki.display.ButtonGadget
 import querki.display.input._
@@ -20,63 +19,6 @@ import querki.editing.EditFunctions
 import querki.editing.EditFunctions._
 import querki.globals._
 import querki.security.SecurityFunctions._
-
-private[security] class PermCheckboxes(allPerms: Seq[PermInfo], thingPropsOpt: Option[Seq[PropValInfo]], std: StandardThings) 
-  (implicit e: Ecology, ctx: Ctx.Owner)
-  extends InputGadget[html.Div](e) with NoAutoSave with ForProp 
-{
-  val prop = std.security.rolePermissionsProp
-  
-  def hook() = {}
-  
-  def values: Seq[String] = 
-    checkboxes
-      .filter(oneChk => $(oneChk.chk.elem).prop("checked").asInstanceOf[Boolean])
-      .map(_.perm.id.underlying)
-  
-  // TODO: I'd be much more comfortable with this if we had a good way to get OIDs instead of names here.
-  // It works the way it does because that's what we get from ThingFunctions.getProperties(), but it
-  // kind of sucks.
-  val thingPermNames: List[String] = {
-    (for {
-      thingProps <- thingPropsOpt
-      rolePermsProp <- thingProps.find(_.propInfo.oid == std.security.rolePermissionsProp.oid)
-    }
-      yield rolePermsProp.raw.lines.toList).getOrElse(List())
-  }
-  
-  def hasPerm(perm: PermInfo) = thingPermNames.contains(perm.name)
-  
-  class OneCheckbox(val perm: PermInfo) extends Gadget[html.Div] {
-    val name = GadgetRef.of[html.Anchor]
-      .whenRendered { g =>
-        $(g.elem).popover(PopoverOptions.trigger(Trigger.click))
-      }
-    val chk = GadgetRef.of[html.Input]
-    
-    def doRender() =
-      div(cls := "row",
-        span(cls := "col-md-1",
-          chk <= input(
-            tpe := "checkbox",
-            if (hasPerm(perm))
-              checked := "checked"
-          )
-        ),
-        name <= a(cls := "", data("toggle") := "popover", data("content") := perm.summary, perm.name)
-      )
-  }
-  
-  val checkboxes = 
-    for (perm <- allPerms)
-      yield new OneCheckbox(perm)
-  
-  def doRender() =
-    div(cls := "container",
-      p(b("This Role gives its members these permissions throughout this Space")),
-      checkboxes
-    )
-}
 
 /**
  * The panel for editing or creating a Role. Don't create this directly; use the helper functions in the
