be522b191f16a6d50a495eb8da16fbb13336646b
Mark "Justin" Waks <jducoeur+github@gmail.com>
2018-09-20 18:56:42 -0400

    Added the special case that, if the caller specifies a Function, it will be evaluated, and the results JSONified. This should hopefully suffice for the Arisia Use Case.

--- querki/scalajvm/app/querki/imexport/JsonFunctionsImpl.scala
+++ querki/scalajvm/app/querki/imexport/JsonFunctionsImpl.scala
@@ -3,9 +3,12 @@ package querki.imexport
 import querki.api.{AutowireParams, SpaceApiImpl}
 import querki.data.TID
 import querki.globals._
+import querki.values.QLRequestContext
 
 class JsonFunctionsImpl(info:AutowireParams)(implicit e:Ecology) extends SpaceApiImpl(info, e) with JsonFunctions  {
+  lazy val Basic = interface[querki.basic.Basic]
   lazy val Core = interface[querki.core.Core]
+  lazy val QL = interface[querki.ql.QL]
   
   def doRoute(req:Request):Future[String] = route[JsonFunctions](this)(req)
   
@@ -14,8 +17,18 @@ class JsonFunctionsImpl(info:AutowireParams)(implicit e:Ecology) extends SpaceAp
     val exporter = new JsonExport
     propIdOpt match {
       case Some(propId) => withProp(propId) { prop =>
+        // They specified a particular Property:
         val qv = thing.getPropVal(prop)
-        fut(exporter.jsonify(qv, false))
+        if (qv.pType == Basic.QLType) {
+          // Special case: if it is a Function, we *process* that, and JSONify the result:
+          val processedOpt = for {
+            ql <- qv.firstTyped(Basic.QLType)
+          }
+            yield QL.processMethod(ql, QLRequestContext(rc), None, Some(thing), None)
+                        
+          processedOpt.map(_.map(processed => exporter.jsonify(processed, false))).getOrElse(fut("null"))
+        } else
+          fut(exporter.jsonify(qv, false))
       }
       case None => {
         // No Property, so render the Thing itself by sending in the OID:
