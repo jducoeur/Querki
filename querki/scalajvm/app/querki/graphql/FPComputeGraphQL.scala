df39845fefb71450f97f77cbfc40e46d7506be1c
Mark "Justin du Coeur" Waks <github@waks.org>
2019-08-11 16:04:41 -0400

    Checkpoint: first compiling and minimal solution for GraphQL
    This is by no means done, but I've now gotten to the bottom of the stack for a few primitive types, and it's all compiling, so let's check it in.

---
+++ querki/scalajvm/app/querki/graphql/FPComputeGraphQL.scala
@@ -0,0 +1,382 @@
+package querki.graphql
+
+import cats.data._
+import cats.data.Validated._
+import cats.effect.IO
+import cats.implicits._
+import models.{Thing, OID, ThingId, PType, Property}
+import play.api.libs.json._
+import querki.core.MOIDs.{QSetOID, QListOID, ExactlyOneOID, OptionalOID}
+import querki.core.MOIDs.{TextTypeOID, LargeTextTypeOID, IntTypeOID, LinkTypeOID}
+import querki.core.QLText
+import querki.globals._
+import querki.values.{PropAndVal, QValue}
+import sangria.ast._
+import sangria.parser.QueryParser
+
+import scala.util.{Success, Failure}
+
+trait JsValueable[VT] {
+  def toJsValue(v: VT): JsValue
+}
+
+object JsValueable {
+  implicit val intJsValueable = new JsValueable[Int] {
+    def toJsValue(v: Int) = JsNumber(v)
+  }
+
+  implicit val textJsValueable = new JsValueable[QLText] {
+    def toJsValue(v: QLText) = JsString(v.text)
+  }
+
+  // TODO: instead of returning the OID, we should dive down the graph here:
+  implicit val linkJsValueable = new JsValueable[OID] {
+    def toJsValue(v: OID): JsValue = JsString(v.toString)
+  }
+
+  implicit class JsValueableOps[T: JsValueable](t: T) {
+    def toJsValue: JsValue = {
+      implicitly[JsValueable[T]].toJsValue(t)
+    }
+  }
+}
+
+class FPComputeGraphQL(implicit state: SpaceState, ecology: Ecology) {
+  import JsValueable._
+
+  final val thingQueryName = "_thing"
+  final val instancesQueryName = "_instances"
+  final val idArgName = "oid"
+  final val nameArgName = "name"
+
+  type SyncRes[T] = ValidatedNec[GraphQLError, T]
+  type Res[T] = EitherT[IO, NonEmptyChain[GraphQLError], T]
+
+  lazy val Core = ecology.api[querki.core.Core]
+
+  def processQuery(query: String): IO[ValidatedNec[GraphQLError, JsValue]] = {
+    val fields: SyncRes[Vector[Field]] =
+      parseQuery(query) andThen
+        (getDefinitions(_)) andThen
+        (_.map(confirmIsQuery(_)).nonEmptySequence) andThen
+        (singleOperation(_)) andThen
+        (_.selections.map(confirmIsField).sequence)
+
+    fields match {
+      case Valid(sels) => {
+        val queryResults: Vector[Res[(String, JsValue)]] = sels.map(processQuerySelection(_))
+        val inverted: Res[Vector[(String, JsValue)]] = queryResults.sequence
+        // Surely we can do this better by defining a Semigroup over JsObject:
+        val result: Res[JsValue] = inverted.map { pairs =>
+          pairs.foldLeft(JsObject(Seq.empty)) { case (obj, (name, jsv)) =>
+            obj + (name -> jsv)
+          }
+        }
+        result.value.map { either =>
+          either.toValidated
+        }
+      }
+      case Invalid(err) => IO.pure(err.invalid)
+    }
+  }
+
+  def parseQuery(query: String): SyncRes[Document] = {
+    QueryParser.parse(query) match {
+      case Success(document) => document.validNec
+      case Failure(ex) => ParseFailure(ex.getMessage).invalidNec
+    }
+  }
+
+  def getDefinitions(doc: Document): SyncRes[NonEmptyList[Definition]] = {
+    NonEmptyList.fromList(doc.definitions.toList) match {
+      case Some(defs) => defs.valid
+      case None => NoDefinitions.invalidNec
+    }
+  }
+
+  def confirmIsQuery(definition: Definition): SyncRes[OperationDefinition] = {
+    definition match {
+      case op: OperationDefinition if (op.operationType == OperationType.Query) => op.valid
+        // TODO: deal with other Operation types!
+      case op: OperationDefinition => UnhandledOperationType(op.operationType).invalidNec
+        // TODO: deal with other Definition types!
+      case _ => UnhandledDefinitionType(definition).invalidNec
+    }
+  }
+
+  def confirmIsField(selection: Selection): SyncRes[Field] = {
+    selection match {
+      case field: Field => field.valid
+      case _ => UnhandledSelectionType(selection).invalidNec
+    }
+  }
+
+  def singleOperation(ops: NonEmptyList[OperationDefinition]): SyncRes[OperationDefinition] = {
+    if (ops.length > 1)
+      TooManyOperations.invalidNec
+    else
+      ops.head.valid
+  }
+
+  /**
+    * Processes a top-level Query selection.
+    *
+    * These are somewhat different, since they follow certain required rules. Lower-level selections are starting
+    * from an existing set of Things, so they have a lot more flexibility.
+    *
+    * This is an IO, because we can have Futures down underneath in the processing.
+    *
+    * This returns a name/value pair. The name is the name of the top-level selection; the value is the contents
+    * of what we found for that selection.
+    */
+  def processQuerySelection(field: Field): Res[(String, JsValue)] = {
+    if (field.name == thingQueryName) {
+      // This is a specific-thing query
+      withThingFromArgument(field, thingQueryName) { thing =>
+        processThing(thing, field)
+      }
+    } else if (field.name == instancesQueryName) {
+      // This is querying all instances of a Model
+      withThingFromArgument(field, instancesQueryName) { model =>
+        val things = state.descendants(model.id, includeModels = false, includeInstances = true).toList
+        val processedThings: Res[List[JsValue]] = things.map { thing =>
+          processThing(thing, field)
+        }.sequence
+        processedThings.map(JsArray(_))
+      }
+    } else {
+      resError(IllegalTopSelection(field.name))
+    }
+  }
+
+  /**
+    * We expect this Field to have either an OID or Name argument, specifying a Thing. Find that Thing, run the
+    * provided processing function, and tuple the result with the selectionName for the resulting JSON.
+    */
+  def withThingFromArgument(field: Field, selectionName: String)(f: Thing => Res[JsValue]): Res[(String, JsValue)] = {
+    for {
+      thing <- getThingFromArgument(field, selectionName).toRes
+      jsValue <- f(thing)
+    }
+      yield (selectionName, jsValue)
+  }
+
+  /**
+    * This field should have an argument specifying a Thing. Find that Thing.
+    */
+  def getThingFromArgument(field: Field, selectionName: String): SyncRes[Thing] = {
+    field.getArgumentStr(idArgName) match {
+      case Valid(Some(oidStr)) => {
+        OID.parseOpt(oidStr) match {
+          case Some(oid) => {
+            state.anything(oid) match {
+              case Some(thing) => thing.valid
+              case None => OIDNotFound(oidStr).invalidNec
+            }
+          }
+          case None => NotAnOID(oidStr).invalidNec
+        }
+      }
+      case Valid(None) => {
+        field.getArgumentStr(nameArgName) match {
+          case Valid(Some(thingName)) => {
+            state.anythingByName(thingName) match {
+              case Some(thing) => thing.valid
+              case None => NameNotFound(thingName).invalidNec
+            }
+          }
+          case Valid(None) => MissingRequiredArgument(field, selectionName, s"$idArgName or $nameArgName").invalidNec
+          case Invalid(err) => err.invalid
+        }
+      }
+      case Invalid(err) => err.invalid
+    }
+  }
+
+  /**
+    * The given Field specifies a Thing. Given that Thing, process its selections, and return the JsValue of the
+    * results.
+    */
+  def processThing(thing: Thing, parentField: Field): Res[JsValue] = {
+    val selectionResults: Res[Vector[(String, JsValue)]] = parentField.selections.map { selection =>
+      for {
+        childField <- confirmIsField(selection).toRes
+        jsValue <- processField(thing, childField)
+      }
+        yield (childField.name, jsValue)
+    }.sequence
+    selectionResults.map { pairs =>
+      val pairMap = pairs.toMap
+      JsObject(pairMap)
+    }
+  }
+
+  /**
+    * Given a Thing, process one Field that represents a Property of that Thing.
+    */
+  def processField(thing: Thing, field: Field): Res[JsValue] = {
+    getProperty(thing, field).toRes.flatMap { prop =>
+      processProperty(thing, prop, field)
+    }
+  }
+
+  def getProperty(thing: Thing, field: Field): SyncRes[Property[_, _]] = {
+    val name = field.name
+    val propOpt = for {
+      thingId <- ThingId.parseOpt(name)
+      thing <- state.anything(thingId)
+      prop <- asProp(thing)
+    }
+      yield prop
+
+    propOpt.syncOrError(UnknownProperty(name))
+  }
+
+  def processProperty(thing: Thing, prop: Property[_, _], field: Field): Res[JsValue] = {
+    // TODO: this is ugly and coded -- can we make it better? Do note that we need to rehydrate the types here
+    // *somehow*, so this isn't trivial:
+    prop.pType.id match {
+      case IntTypeOID => processTypedProperty(thing, prop.confirmType(Core.IntType), Core.IntType, field)
+      case TextTypeOID => processTypedProperty(thing, prop.confirmType(Core.TextType), Core.TextType, field)
+      case LargeTextTypeOID => processTypedProperty(thing, prop.confirmType(Core.LargeTextType), Core.LargeTextType, field)
+      case LinkTypeOID => processTypedProperty(thing, prop.confirmType(Core.LinkType), Core.LinkType, field)
+        // TODO: More types!
+    }
+  }
+
+  def processTypedProperty[VT: JsValueable](thing: Thing, propOpt: Option[Property[VT, _]], pt: PType[VT], field: Field): Res[JsValue] = {
+    val resultOpt: Option[Res[JsValue]] = propOpt.map { prop =>
+      thing.getPropOpt(prop) match {
+        case Some(propAndVal) => processValues(thing, prop, propAndVal.rawList, field)
+        case None => resError(PropertyNotOnThing(thing, prop))
+      }
+    }
+    resultOpt.getOrElse(resError(InternalGraphQLError("Hit a Property whose type doesn't confirm!")))
+  }
+
+  def processValues[VT: JsValueable](thing: Thing, prop: Property[VT, _], vs: List[VT], field: Field): Res[JsValue] = {
+    // What we return depends on the Collection of this Property:
+    prop.cType match {
+      case ExactlyOneOID => {
+        if (vs.isEmpty) {
+          resError(MissingRequiredValue(thing, prop))
+        } else {
+          res(vs.head.toJsValue)
+        }
+      }
+      case OptionalOID => {
+        vs.headOption match {
+          case Some(v) => res(v.toJsValue)
+            // TODO: check the GraphQL spec -- is JsNull correct here?
+          case None => res(JsNull)
+        }
+      }
+      case QListOID | QSetOID => {
+        val jsvs = vs.map(_.toJsValue)
+        res(JsArray(jsvs))
+      }
+    }
+  }
+
+  def res[T](v: T): Res[T] = {
+    EitherT.rightT(v)
+  }
+  def resError[T](err: => GraphQLError): Res[T] = {
+    EitherT.leftT(NonEmptyChain(err))
+  }
+
+  def asProp(thing: Thing): Option[Property[_, _]] = {
+    thing match {
+      case t: Property[_, _] => Some(t)
+      case _ => None
+    }
+  }
+
+  implicit class RichOption[T](tOpt: Option[T]) {
+    def syncOrError(err: => GraphQLError): SyncRes[T] = {
+      tOpt match {
+        case Some(t) => t.valid
+        case None => err.invalidNec
+      }
+    }
+  }
+
+  implicit class RichField(field: Field) {
+    def getArgumentStr(name: String): SyncRes[Option[String]] = {
+      field.arguments.find(_.name == name) match {
+        case Some(arg) => arg.value match {
+          case v: StringValue => Some(v.value).valid
+          case _ => UnhandledArgumentType(arg).invalidNec
+        }
+        case None => None.valid
+      }
+    }
+    def getRequiredArgumentStr(name: String): SyncRes[String] = {
+      getArgumentStr(name) andThen {
+        _ match {
+          case Some(v) => v.valid
+          case None => MissingRequiredArgument(field, name, "string").invalidNec
+        }
+      }
+    }
+  }
+
+  implicit class RichSyncRes[T](syncRes: SyncRes[T]) {
+    def toRes: Res[T] = {
+      syncRes match {
+        case Valid(t) => EitherT.rightT(t)
+        case Invalid(e) => EitherT.leftT(e)
+      }
+    }
+  }
+}
+
+sealed trait GraphQLError {
+  def msg: String
+}
+case class ParseFailure(msg: String) extends GraphQLError
+case object NoDefinitions extends GraphQLError { val msg = "No Definitions provided in the GraphQL Document!" }
+case class UnhandledOperationType(opType: OperationType) extends GraphQLError {
+  val msg = s"Querki does not yet deal with ${opType.getClass.getSimpleName} operations; sorry."
+}
+case class UnhandledDefinitionType(definition: Definition) extends GraphQLError {
+  def msg = s"Querki can not yet deal with ${definition.getClass.getSimpleName}"
+}
+case object TooManyOperations extends GraphQLError { val msg = "Querki can currently only deal with one Query at a time; sorry." }
+case class UnhandledSelectionType(selection: Selection) extends GraphQLError {
+  def msg = s"Querki can not yet deal with ${selection.getClass.getSimpleName} selections; sorry."
+}
+case class UnhandledArgumentType(arg: Argument) extends GraphQLError {
+  def msg = s"Querki can not yet deal with ${arg.value.getClass.getSimpleName} arguments there; sorry."
+}
+case class MissingRequiredArgument(field: Field, name: String, tpe: String) extends GraphQLError {
+  def msg = s"Field ${field.name} requires a $tpe argument named '$name'"
+}
+case class UnknownThing(name: String) extends GraphQLError {
+  def msg = s"There is no Thing named $name"
+}
+case class IllegalTopSelection(name: String) extends GraphQLError {
+  def msg = s"The top level of a GraphQL query must be _thing or _instances."
+}
+case class NotAnOID(str: String) extends GraphQLError {
+  def msg = s"$str is not a valid OID"
+}
+case class OIDNotFound(oid: String) extends GraphQLError {
+  def msg = s"No Thing found with OID $oid"
+}
+case class NameNotFound(name: String) extends GraphQLError {
+  def msg = s"No Thing found named $name -- maybe that isn't the correct Link Name?"
+}
+case class UnknownProperty(name: String) extends GraphQLError {
+  def msg = s"Unknown Property: $name"
+}
+case class PropertyNotOnThing(thing: Thing, prop: AnyProp) extends GraphQLError {
+  def msg = s"${thing.displayName} does not have requested property ${prop.displayName}"
+}
+case class UnsupportedType(prop: Property[_, _]) extends GraphQLError {
+  def msg = s"Querki GraphQL does not yet support ${prop.pType.displayName} properties; sorry."
+}
+case class InternalGraphQLError(msg: String) extends GraphQLError
+case class MissingRequiredValue(thing: Thing, prop: Property[_, _]) extends GraphQLError {
+  def msg = s"Required Property ${prop.displayName} on Thing ${thing.displayName} is empty!"
+}
