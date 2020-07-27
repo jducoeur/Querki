444c46e44755746e80c6b8ccb5bbeff394c4b168
Mark "Justin du Coeur" Waks <github@waks.org>
2020-07-26 20:35:02 -0400

    Tweak NotificationMidFuncs to compile properly
    Not sure why this didn't cause me problems before...

--- querki/scalajvm/test/querki/notifications/NotificationMidFuncs.scala
+++ querki/scalajvm/test/querki/notifications/NotificationMidFuncs.scala
@@ -3,6 +3,7 @@ package querki.notifications
 import autowire._
 import org.scalactic.source.Position
 import org.scalatest.Matchers._
+import querki.globals.execContext
 import querki.test.mid.ClientState.withUser
 import querki.test.mid._
 
