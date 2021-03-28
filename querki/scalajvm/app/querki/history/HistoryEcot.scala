115a3377b251d9dd261930caa824e0655b53b2dc
Mark "Justin du Coeur" Waks <github@waks.org>
2021-03-28 16:57:06 -0400

    QI.7w4ger9, QI.7w4gerb -- add _listDeletedThings()
    This is a new function that returns a list of all the Things in the Space that have been deleted, optionally including a predicate to use as a filter.
    
    The tickets aren't complete yet -- it needs to be made more powerful (including the OID and deletion time); we need a default page for this; and it needs tests. But the core functionality is now working.
    
    Also includes a bunch of reformatting, since I'm getting serious about scalafmt. There's going to be a lot of that to come.

--- querki/scalajvm/app/querki/history/HistoryEcot.scala
+++ querki/scalajvm/app/querki/history/HistoryEcot.scala
@@ -6,60 +6,64 @@ import querki.globals._
 import querki.values.RequestContext
 import HistoryFunctions._
 import models.OID
-import querki.history.SpaceHistory.RestoreDeletedThing
-import querki.ql.{QLExp, QLThingId, QLCall}
-import querki.spaces.messages.{ThingFound, SpaceSubsystemRequest}
-import querki.util.{PublicException, ActorHelpers}
+import querki.core.QLText
+import querki.history.SpaceHistory.{DeletedThings, ForAllDeletedThings, RestoreDeletedThing}
+import querki.ql.{ParsedQLText, QLCall, QLExp, QLThingId}
+import querki.spaces.messages.{SpaceSubsystemRequest, ThingFound}
+import querki.util.{ActorHelpers, PublicException}
 
 object MOIDs extends EcotIds(65) {
   val FindAllStompedCmdOID = moid(1)
   val HistoryPermOID = moid(2)
   val UndeleteFunctionOID = moid(3)
+  val ListDeletedThingsOID = moid(4)
 }
 
-class HistoryEcot(e:Ecology) extends QuerkiEcot(e) with History with querki.core.MethodDefs {
+class HistoryEcot(e: Ecology) extends QuerkiEcot(e) with History with querki.core.MethodDefs {
   import MOIDs._
-  
+
   val cmds = new HistoryCommands(e)
-  
+
   val AccessControl = initRequires[querki.security.AccessControl]
-  
+
   lazy val ApiRegistry = interface[querki.api.ApiRegistry]
   lazy val SpaceOps = interface[querki.spaces.SpaceOps]
 
   implicit val timeout = ActorHelpers.timeout
 
   override def postInit() = {
-    ApiRegistry.registerApiImplFor[HistoryFunctions, HistoryFunctionsImpl](SpaceOps.spaceRegion, allowedDuringHistory = true)
+    ApiRegistry
+      .registerApiImplFor[HistoryFunctions, HistoryFunctionsImpl](SpaceOps.spaceRegion, allowedDuringHistory = true)
   }
-  
-  def viewingHistoryVersion(rc:RequestContext):Option[HistoryVersion] = {
+
+  def viewingHistoryVersion(rc: RequestContext): Option[HistoryVersion] = {
     rc.rawParam("_historyVersion") match {
-      case Some(vStr) => {
+      case Some(vStr) =>
         Some(vStr.toLong)
-      }
       case _ => None
     }
   }
-  
-  def isViewingHistory(rc:RequestContext):Boolean = {
+
+  def isViewingHistory(rc: RequestContext): Boolean = {
     rc.rawParam("_historyVersion").map(_.length > 0).getOrElse(false)
   }
 
   lazy val HistoryPerm = AccessControl.definePermission(
     HistoryPermOID,
-    "Can View History", 
-    "This permission controls whether someone is allowed to examine the History of this Space. This gives full read permission!", 
+    "Can View History",
+    "This permission controls whether someone is allowed to examine the History of this Space. This gives full read permission!",
     Seq(AccessControl.OwnerTag),
     Seq(AccessControl.AppliesToSpace),
-    false, 
-    false)
+    false,
+    false
+  )
 
-  /***********************************************
-   * FUNCTIONS
-   ***********************************************/
+  ///////////////////////////////////
+  // FUNCTIONS
+  ///////////////////////////////////
 
-  lazy val UndeleteFunction = new InternalMethod(UndeleteFunctionOID,
+  lazy val UndeleteFunction = new InternalMethod(
+    UndeleteFunctionOID,
     toProps(
       setName("_undeleteThing"),
       Summary("Given the OID of a deleted Thing, restore it"),
@@ -76,9 +80,11 @@ class HistoryEcot(e:Ecology) extends QuerkiEcot(e) with History with querki.core
           |That's necessarily true (you can't pass in the OID, since the Thing doesn't exist any more).
           |
           |This is primarily intended for internal use, from the Deleted Things page.
-          |""".stripMargin)
+          |""".stripMargin
+      )
     )
   ) {
+
     // Since the Thing presumably doesn't exist in this Space currently, it would be illegal to process it in
     // the normal way -- we would get a Thing Not Found error. Instead, we have to grovel down through the
     // raw parse tree to fetch the OID itself:
@@ -88,34 +94,68 @@ class HistoryEcot(e:Ecology) extends QuerkiEcot(e) with History with querki.core
         op <- phrase.ops.headOption
         call <- op match {
           case call: QLCall => Some(call)
-          case _ => None
+          case _            => None
         }
         thingIdStr <- call.name match {
           case QLThingId(n) => Some(n)
-          case _ => None
+          case _            => None
         }
         oid <- OID.parseOpt(thingIdStr)
-      }
-        yield oid
+      } yield oid
     }
 
-    override def qlApply(inv:Invocation): QFut = {
+    override def qlApply(inv: Invocation): QFut = {
       val user = inv.context.request.requesterOrAnon
       for {
         expOpt <- inv.rawParam("oid")
         exp <- inv.opt(expOpt, Some(PublicException("History.undeleteNoOid")))
         oid <- inv.opt(expToOid(exp), Some(PublicException("History.undeleteNotOid", exp.reconstructString)))
-        ThingFound(id, newState) <-
-          inv.fut(SpaceOps.spaceRegion ?
-            SpaceSubsystemRequest(inv.context.request.requesterOrAnon, inv.state.id,
-              RestoreDeletedThing(user, oid)))
-      }
-        yield ExactlyOne(LinkType(id))
+        ThingFound(id, newState) <- inv.fut(
+          SpaceOps.spaceRegion ?
+            SpaceSubsystemRequest(inv.context.request.requesterOrAnon, inv.state.id, RestoreDeletedThing(user, oid))
+        )
+      } yield ExactlyOne(LinkType(id))
+    }
+  }
+
+  lazy val ListDeletedThings = new InternalMethod(
+    ListDeletedThingsOID,
+    toProps(
+      setName("_listDeletedThings"),
+      Summary("Lists the Things in this Space that pass the given predicate, that have been deleted."),
+      Signature(
+        expected = None,
+        reqs = Seq.empty,
+        opts = Seq(
+          ("predicate", AnyType, Core.QNone, "A QL expression returning YesOrNoType, to try on each deleted Thing")
+        ),
+        returns = (AnyType, "A List of the deleted Things")
+      ),
+      Details("""If no predicate is given, all deleted Things will be returned.""")
+    )
+  ) {
+
+    override def qlApply(inv: Invocation): QFut = {
+      for {
+        rawOpt <- inv.rawParam("predicate")
+        qlText = rawOpt.map(raw => QLText(raw.reconstructStandalone))
+        DeletedThings(things) <- inv.fut(
+          SpaceOps.spaceRegion ?
+            SpaceSubsystemRequest(
+              inv.context.request.requesterOrAnon,
+              inv.state.id,
+              ForAllDeletedThings(inv.context.request, qlText)
+            )
+        )
+        thing <- inv.iter(things)
+        // TODO: create a more useful return display
+      } yield ExactlyOne(TextType(thing.display.str))
     }
   }
 
   override lazy val props = Seq(
     HistoryPerm,
-    UndeleteFunction
+    UndeleteFunction,
+    ListDeletedThings
   )
 }
