8abafd9a0911e00d5612cc58ffe05df5ef086038
Mark "Justin du Coeur" Waks <github@waks.org>
2021-05-15 16:34:34 -0400

    QI.bu6oguy: deal with spurious timeout errors
    The previous commit got to the point where uploads were working, but we were getting spurious errors along the way, because the frontend's attempts to fetch the state were splashing against the Actor, which was spending all its time parsing the Space. (Which in the case of the Playtest Space, takes a good 20 seconds.) So I moved the parse task into a Future, looping back when done -- that way, it still takes a while, but doesn't kick out errors in the process.
    
    Also improved the progress messages. It looks like I built that system halfway, but never properly finished it because the gap was never apparently in little Spaces. It matters for huge ones, though.
    
    The result of this is that the whole process is now async, so we can get rid of the vestigial synchronous try/catch -- all errors are now coming via the async channel.

--- querki/scalajvm/app/querki/imexport/ImportSpaceActor.scala
+++ querki/scalajvm/app/querki/imexport/ImportSpaceActor.scala
@@ -62,7 +62,10 @@ class ImportSpaceActor(
   }
   lazy val persister = SpacePersistenceFactory.getSpaceManagerPersister(context)
 
-  def buildSpaceState(rc: RequestContext): SpaceState = {
+  def buildSpaceState(
+    rc: RequestContext,
+    uploaded: String
+  ): SpaceState = {
     importType match {
       case ImportXML => {
         val xml = uploaded
@@ -81,23 +84,34 @@ class ImportSpaceActor(
     }
   }
 
+  /**
+   * Since building the space state can take a *long* time (we have spaces whose XML is north of 7 Meg, and take
+   * a good 20 seconds to parse), we break those out into a Future wrapped by a RequestM, so the process doesn't
+   * block the Actor.
+   */
+  def wrappedBuildSpaceState(rc: RequestContext): RequestM[SpaceState] = {
+    // Run it through a loopback so that it comes back in the Actor's thread context:
+    loopback(Future(buildSpaceState(rc, uploaded)))
+  }
+
   override def receive = handleChunks.orElse {
     case GetProgress => {
       // The client is asking for an update, so calculate where we are:
-      if (!uploadComplete) {
-        // We arbitrarily count the uploading as the first 50% of the total process:
-        val percent = ((uploaded.size / totalSize) * 50).toInt
-        sender ! ImportProgress("Uploading...", percent, spaceInfo, failed)
-      } else {
-        // We're into processing.
-        sender ! ImportProgress("Processing...", processPercent, spaceInfo, failed)
-      }
+      val percent =
+        if (!uploadComplete) {
+          // We arbitrarily count the uploading as the first 50% of the total process:
+          ((uploaded.size / totalSize) * 50).toInt
+        } else {
+          // We're into processing.
+          processPercent
+        }
+      sender ! ImportProgress(importMsg, percent, spaceInfo, failed)
     }
 
     case CompletionAcknowledged => context.stop(self)
   }
 
-  var importMsg: String = "Uploading"
+  var importMsg: String = "Uploading..."
 
   var processPercent: Int = 0
 
@@ -112,7 +126,9 @@ class ImportSpaceActor(
   }
 
   def processBuffer(rc: RequestContext) = {
+    // TODO: oh god, this is horrible. There is a State monad fighting to break out of this code when I have time...
     processPercent = 50
+    importMsg = "Done uploading; constructing the new Space's data (this may take a while)..."
 
     if (rc.requester.isEmpty)
       throw new Exception("Somehow got to ImportSpaceActor when not logged in!")
@@ -129,12 +145,13 @@ class ImportSpaceActor(
       failed = true
     }
 
-    try {
+    val result: RequestM[Boolean] = for {
       // Given the XML or MySQL file, build a SpaceState:
-      val rawState = buildSpaceState(rc)
-      processPercent = 60
-      val canon = canonicalize(name)
-      val renamedState = rawState.copy(
+      rawState <- wrappedBuildSpaceState(rc)
+      _ = processPercent = 60
+      _ = importMsg = "Remapping object IDs..."
+      canon = canonicalize(name)
+      renamedState = rawState.copy(
         name = canon,
         pf = rawState.props ++
           toProps(
@@ -142,46 +159,42 @@ class ImportSpaceActor(
             Basic.DisplayNameProp(name)
           )
       )
-      val user = rc.requesterOrAnon
+      user = rc.requesterOrAnon
+      // Remap all of the OIDs in the SpaceState to use new ones:
+      (remappedState, oidMap) <- remapOIDs(renamedState)
+      _ = processPercent = 70
+      _ = importMsg = "Creating the Space..."
+      // This just creates the Space in MySQL, but doesn't boot it yet:
+      newSpaceId <- createSpace(user, remappedState.id, canon, name, StatusInitializing)
+      _ = processPercent = 80
+      _ = importMsg = "Booting the new Space..."
+      // Boot the new Space *locally*, so we can throw the State over the wall:
+      spaceActor = startSpaceActor(newSpaceId)
+      // Slam the State:
+      ThingFound(_, finalState) <- spaceActor.request(SetState(
+        user,
+        newSpaceId,
+        remappedState,
+        SetStateReason.ImportedFromExport,
+        rawState.displayName
+      ))
+      _ = processPercent = 90
+      // Finally, once it's all built, shut down this temp Actor:
+      _ = context.stop(spaceActor)
+      // And tell the SpaceManager that this Space is now ready to be treated normally:
+      _ <- SpaceOps.spaceManager.requestFor[StatusChanged.type](ChangeSpaceStatus(newSpaceId, StatusNormal))
+    } yield {
+      // We don't actually do anything -- we wait for the client to request an update
+      // on the situation. So just note that we're done.
+      spaceInfo = Some(ClientApi.spaceInfo(remappedState, user))
 
-      val result: RequestM[Boolean] = for {
-        // Remap all of the OIDs in the SpaceState to use new ones:
-        (remappedState, oidMap) <- remapOIDs(renamedState)
-        _ = processPercent = 70
-        // This just creates the Space in MySQL, but doesn't boot it yet:
-        newSpaceId <- createSpace(user, remappedState.id, canon, name, StatusInitializing)
-        _ = processPercent = 80
-        // Boot the new Space *locally*, so we can throw the State over the wall:
-        spaceActor = startSpaceActor(newSpaceId)
-        // Slam the State:
-        ThingFound(_, finalState) <- spaceActor.request(SetState(
-          user,
-          newSpaceId,
-          remappedState,
-          SetStateReason.ImportedFromExport,
-          rawState.displayName
-        ))
-        _ = processPercent = 90
-        // Finally, once it's all built, shut down this temp Actor:
-        _ = context.stop(spaceActor)
-        // And tell the SpaceManager that this Space is now ready to be treated normally:
-        _ <- SpaceOps.spaceManager.requestFor[StatusChanged.type](ChangeSpaceStatus(newSpaceId, StatusNormal))
-      } yield {
-        // We don't actually do anything -- we wait for the client to request an update
-        // on the situation. So just note that we're done.
-        spaceInfo = Some(ClientApi.spaceInfo(remappedState, user))
+      true
+    }
 
-        true
+    result.onFailure {
+      case ex: Exception => {
+        handleError(ex)
       }
-
-      result.onFailure {
-        case ex: Exception => {
-          handleError(ex)
-        }
-      }
-    } catch {
-      // Caught an error while trying to build the SpaceState:
-      case ex: Throwable => handleError(ex)
     }
 
     sender ! UploadProcessSuccessful("Processing in progress")
